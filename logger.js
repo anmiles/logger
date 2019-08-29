8904af571d2ee85bc027f575348c6534aa99507e
Anatoliy Oblauhov <anmiles@wildapricot.com>
2019-08-30 00:23:41 +0300

    Refactor structure

---
+++ logger.js
@@ -0,0 +1,89 @@
+var fs = require('fs');
+var clc = require('cli-color');
+var moment = require('moment');
+require('json-beautify');
+require('prototypes');
+
+function Logger() {
+    var logger = this;
+    var logDir;
+    var logFile;
+    var extension = "log";
+    logger.showDebug = false;
+
+    function log(str, modifier, showStack, skipDebug) {
+        if (showStack) {
+            str = str.stack ? str.stack : new Error(beautify(str)).stack.replace(/^Error/, '');
+        } else {
+            str = beautify(str);
+        }
+
+        str = logger.time() + '\t' + str;
+        if (logger.showDebug || !skipDebug) console.log(typeof modifier == 'function' ? modifier(str) : str);
+
+        if (!logDir) {
+            logger.dir('./logs');
+        }
+
+        fs.ensureDirectory(logDir);
+        fs.appendFileSync(logFile, str + '\r\n');
+    }
+    
+    function beautify(str) {
+        return JSON.isScalar(str) ? str : JSON.beautify(str);
+    }
+
+    logger.dir = function(logPath, groupByDate) {
+        var callerfile = fs.getFileName(fs.getCallerFile());
+
+        if (groupByDate) {
+            logDir = logPath + moment().format('YYYY/MM/DD');
+            logFile = '{0}/{1}.{2}.{3}'.format(logDir, callerfile, moment().format('HH.mm.ss.SSS'), extension);
+        } else {
+            logDir = logPath;
+            logFile = '{0}/{1}.{2}.{3}'.format(logDir, callerfile, logger.time(true), extension);
+        }
+
+        process.on('uncaughtException', function(err) {
+            logger.error(err);
+        });
+    };
+
+    logger.log = function(str, showStack) {
+        log(str, null, typeof showStack == 'undefined' ? false : showStack);
+    };
+
+    logger.info = function(str, showStack) {
+        log(str, clc.green, typeof showStack == 'undefined' ? false : showStack);
+    };
+
+    logger.warn = function(str, showStack) {
+        log(str, clc.yellow.bold, typeof showStack == 'undefined' ? false : showStack);
+    };
+
+    logger.error = function(str, showStack) {
+        log(str, clc.red.bold, typeof showStack == 'undefined' ? true : showStack);
+    };
+
+    logger.stop = function(str) {
+        throw str;
+    }
+
+    logger.trace = function() {
+        log('', clc.cyan, true);
+    };
+
+    logger.debug = function(str, showStack) {
+        log(str, null, showStack, true);
+    };
+
+    logger.time = function(safe) {
+        return moment().format(safe ? 'YYYY.MM.DD_HH.mm.ss.SSS' : 'YYYY-MM-DD HH:mm:ss.SSS');
+    };
+    
+    logger.clear = function() {
+        fs.deleteFolderRecursive(logDir);
+    };    
+}
+
+module.exports = new Logger();
